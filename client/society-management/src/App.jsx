61390d5d7a962eebe0bd8b858929037d5407a72e
Sarthak Chakraborty <csarthakcal@gmail.com>
2025-07-02 12:25:02 +0530

    Setup

---
+++ client/society-management/src/App.jsx
@@ -0,0 +1,15 @@
+import {} from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Society Management</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
