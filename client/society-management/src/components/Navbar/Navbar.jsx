39a06262de6e0d4060f64000a3d9937f22f93361
Suvajit <suvajitroy.student@gmail.com>
2025-07-02 13:09:22 +0530

    testing v3

--- client/society-management/src/components/Navbar/Navbar.jsx
+++ client/society-management/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-      <h1>Hello World, Testing 123!!</h1>
+      <h1>Hello World, Testing 12345!!</h1>
     </div>
   )
 }
