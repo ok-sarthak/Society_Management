fd6a0400bd6563ff03a9f9eefd5db5fe71eb8302
Suvajit <suvajitroy.student@gmail.com>
2025-07-02 22:31:55 +0530

    routes updated for all the pages

--- client/society-management/src/App.jsx
+++ client/society-management/src/App.jsx
@@ -1,6 +1,13 @@
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/footer';
+import About from './pages/About/aboutPage';
+import MemberLogin from './pages/MemberLoginPage/MemberLogin';
+import MemberSignup from './pages/MemberLoginPage/MemberSignup';
+import ForgotPassword from './pages/MemberLoginPage/ForgotPassword';
+import AdminLogin from './pages/AdminLoginPage/AdminLogin';
+import AdminSignup from './pages/AdminLoginPage/AdminSignup';
+import AdminForgotPassword from './pages/AdminLoginPage/AdminForgotPassword';
 import './index.css'
 
 import { Routes, Route } from 'react-router-dom';
@@ -9,6 +16,13 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<><Navbar/><Hero/> <Footer/></>} />
+      <Route path="/about" element={<><Navbar/><About/><Footer/></>} />
+      <Route path="/member/login" element={<MemberLogin/>} />
+      <Route path="/member/signup" element={<MemberSignup/>} />
+      <Route path="/member/forgot-password" element={<ForgotPassword/>} />
+      <Route path="/admin/login" element={<AdminLogin/>} />
+      <Route path="/admin/signup" element={<AdminSignup/>} />
+      <Route path="/admin/forgot-password" element={<AdminForgotPassword/>} />
     </Routes>
   );
 };
